50c1026a0187df109b5c7a30a18a365e4b2bd648
Lauren Saunders <lsaund11@uw.edu>
2016-03-08 22:40:25 -0800

    working on graphing mean FPKM values

--- diffexpress.m
+++ diffexpress.m
@@ -14,3 +14,24 @@ unablated=melFPKM(7:12,:);
 
 % Also, need read count data to normalize and everything that follows from
 % that.
+
+%% To plot the mean FPKM values between the two groups
+
+mF2 = melFPKM'; % it didn't work with the non-transposed version
+
+% find mean (with transpose)
+meanUA = mean(mF2(:, 7:12),2);
+meanA = mean(mF2(:, 1:6),2);
+
+% find dispersion (with transpose)
+dispUA = std(mF2(:,7:12),0,2) ./ meanUA;
+dispA = std(mF2(:,1:6),0,2) ./ meanA;
+
+% plot on a log-log scale (with transpose)
+figure;
+loglog(meanUA, dispUA, '.r');
+hold on;
+loglog(meanA, dispA, '.b');
+xlabel('log2(mean)');
+ylabel('log2(Dispersion)');
+legend('UnAblated', 'Ablated', 'Location', 'southwest');
